182ca8d445ab28ed32632ae897a82f1c4e0840d0
SyedaRida707 <syedarida953@gmail.com>
2026-07-17 20:51:00 +0500

    done

--- app.js
+++ app.js
@@ -32,7 +32,7 @@
 
 
 
-// let :  is a block scope variable we can not accessed outside the block if we create outside the block we can accessed inside the block. in let we canot redeclare , just reassign without let keyword.
+// let :  is a block scope variable we can not accessed outside the block if we create outside the block we can accessed inside the block. in let we cannot redeclare , just reassign without let keyword.
 
 // let $userName = 'Rida Abid';   // Declare
 // console.log($userName);
@@ -50,6 +50,7 @@
 // }
 // showName()
 // console.log($name);
+
 
 // let b; //declare
 // {
@@ -76,6 +77,116 @@
 //     console.log($name);
 // }
 // console.log($name);
+
+
+
+// ===================================== Hoisting =====================================
+// Execution se pehle hi woh declarations ko memory mein register kar leti hai (hoisting), phir code line by line execute hota hai.
+// // Hoisting means JavaScript processes variable declarations
+// before executing the code.
+
+// ---------------------- var ----------------------
+
+// Variables declared with var are hoisted and initialized with undefined.
+// So accessing them before their declaration does not give an error;
+// it returns undefined.
+
+// console.log(a); // undefined
+// var a = 10;
+
+// ---------------------- let ----------------------
+
+// Variables declared with let are also hoisted,
+// but they are NOT initialized.
+// They remain in the Temporal Dead Zone (TDZ)
+// until the declaration line is executed.
+
+// console.log(a); // ReferenceError
+// let a = 10;
+
+// ---------------------- const ----------------------
+
+// const is also hoisted but remains in the
+// Temporal Dead Zone until its declaration.
+// Accessing it before declaration throws a ReferenceError.
+
+// console.log(a); // ReferenceError
+// const a = 10;
+
+
+
+// <==============================Template Literals==============================================>
+// let $userName = 'Rida Abid';
+// console.log('This is not easy to use concatination '+$userName); //Concatination
+// console.log(`This is the easy way to use template literals ${$userName}`); //Template literals
+
+
+
+
+// <================================Data Types in Js==============================================>
+// Data Types => variables store which type of value
+// primitive data type => store single value
+
+// string
+// let $string = 'Syeda Rida';
+// console.log($string, typeof  $string);
+// console.log(`${$string} ${typeof $string}`);
+
+
+// number
+// let $number = 200;
+// console.log($number, typeof $number);
+
+// boolean
+// let $boolean = true;       // true = 1, false = 0
+// console.log($boolean, typeof $boolean);
+
+// let $null = null;
+// console.log(null,typeof $null);  // type of null is object
+
+// // undefined
+// let $undefined ;
+// console.log(typeof $undefined);
+
+// bigint
+// let $bigint = 10000000000000000000000000000000000000000000000000000n;
+// console.log($bigint, typeof $bigint);
+
+// symbol
+// let $symbol = Symbol('Rida Abid');
+// console.log($symbol, typeof $symbol);
+// let $id1 = Symbol('id');
+// let $id2 = Symbol('id');
+// console.log($id1 === $id2);
+
+// non-primitive data type = store multiple values
+
+// array
+// let $array = [10,20,30];
+// console.log($array, typeof $array);
+
+
+// object
+// let $Object = {
+//     name : 'Abid',
+//     age : '50'
+// }
+// console.log($Object, typeof $Object);
+
+// function
+// function showName(){
+// }
+// console.log(typeof showName);
+
+
+
+
+
+
+
+
+
+
 
 
 
