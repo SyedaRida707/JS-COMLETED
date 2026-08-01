0c2208c7743108e13694872889829d0962b26289
SyedaRida707 <syedarida953@gmail.com>
2026-08-01 23:36:17 +0500

    done

---
+++ array.js
@@ -0,0 +1,26 @@
+// <==================== ARRAY =====================>
+// an array store multiple values in a single variable  
+// store different data type 
+// In JavaScript, an array can store any type of value, 
+// including numbers, strings, booleans, objects, functions, and even other arrays.
+
+// let num = [1, 2, 3, 4];                      //num
+// let name = ['atruba', 'rida'];               //string
+// let arrayInarray = [[2, 5, 8]];              //array inside array
+// let booleanValue = [true, false];            //boolean
+// let studentInfor = [{
+//     name: 'rida',
+//     age: 21
+// }]                                           //object
+
+// let arrayFunc = [function () {               // function in array
+//     console.log('hi');
+// }]
+
+
+// console.log(num[3]);
+// console.log(name[0]);
+// console.log(arrayInarray[0][0]);
+// console.log(booleanValue[0]);
+// console.log(studentInfor[0].name);
+// arrayFunc[0]();
