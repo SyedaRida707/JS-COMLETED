fa6d5bde1b1467bfd211db61efaaf70d5fbe0e16
SyedaRida707 <syedarida953@gmail.com>
2026-08-02 19:25:10 +0500

    done

--- array.js
+++ array.js
@@ -1,12 +1,12 @@
 // <==================== ARRAY =====================>
-// an array store multiple values in a single variable  
-// store different data type 
-// In JavaScript, an array can store any type of value, 
+// an array store multiple values in a single variable
+// store different data type
+// In JavaScript, an array can store any type of value,
 // including numbers, strings, booleans, objects, functions, and even other arrays.
 
 // let num = [1, 2, 3, 4];                      //num
 // let name = ['atruba', 'rida'];               //string
-// let arrayInarray = [[2, 5, 8]];              //array inside array
+// let arrayInarray = [[2, 5, 8]];              //array inside array or (multi diemension array)
 // let booleanValue = [true, false];            //boolean
 // let studentInfor = [{
 //     name: 'rida',
@@ -24,3 +24,81 @@
 // console.log(booleanValue[0]);
 // console.log(studentInfor[0].name);
 // arrayFunc[0]();
+
+
+// <=========================================>
+
+// TASk 1
+// let fruits = ['mango 🥭', 'cherry 🍒', 'apple 🍎', 'strawberry 🍓'];
+// for (let i = 0; i < fruits.length; i++) {
+//     console.log(fruits[i]);
+// }
+
+
+// <=========================================>
+
+// TASk 2 reverse nikalo
+// let fruits = ['mango 🥭', 'cherry 🍒', 'apple 🍎', 'strawberry 🍓'];
+// for (let i = fruits.length - 1; i >= 0; i--) {
+//     console.log(fruits[i]);
+// }
+
+
+// <=========================================>
+
+
+// TASk 3
+// use of for of loop
+// let nums = [10, 20, 30, 40, 50];
+// let sum = 0;
+// for(let i of nums){
+//     sum += i;
+//     console.log(i);
+// }
+// console.log(sum);
+
+
+// <=========================================>
+// use of for in loop
+
+// for (let i in nums) {
+//     sum += nums[i]
+//     console.log(nums[i]);
+// }
+// console.log(sum);
+
+
+
+// <=========================================>
+
+
+// TASk 4
+// let nums = [10, 20, 23, 55, 99, 77, 66, 44, 56, 21];
+// for (let i of nums) {
+//     if (i % 2 === 0) {
+//         console.log('its even num',i);
+//     }
+// }
+
+
+// <=========================================>
+
+
+// TASk 5
+// Change position of index 1
+// let nums = [10, 20, 30];
+// console.log(nums[1]);
+// nums[1] = 40;
+// console.log(nums[1]);
+
+
+
+// <==================    METHODS   =======================>
+//Mutating methods means modifying original array
+
+// push
+// (Adds elements to the end of an array);
+// (Changes the original array.)
+// (Returns the new length of the array.)
+
+
