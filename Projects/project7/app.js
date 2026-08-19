c1b2d54bd9751ca9a702e35cdea07c1a64f30d2b
SyedaRida707 <syedarida953@gmail.com>
2026-08-19 19:25:47 +0500

    completed

--- Projects/project7/app.js
+++ Projects/project7/app.js
@@ -1,17 +1,56 @@
-let allQ = document.querySelectorAll('.questions h2');
-// console.log(allQ);
+// let allQ = document.querySelectorAll('.questions h2');
 
-allQ.forEach((Q, I) => {
-    // console.log(Q, I);
+// first way
+// allQ.forEach((Q, I) => {
 
-    Q.addEventListener('click', () => {
-        allQ.forEach((h, i) => {
-            // console.log(h.innerHTML);
-            // console.log(h.nextElementSibling);
-            // console.log(h, i);
-            
-            
-        });
-    });
-});
+//     Q.addEventListener('click', () => {
+
+//         allQ.forEach((q, i) => {
+//             console.log(q, i, 'click index', I);
+//             if (i != I) {
+//                 q.lastElementChild.innerHTML = '❌';
+//                 q.nextElementSibling.classList.remove('showans');
+//             }
+
+//         });
+
+//         if (Q.lastElementChild.innerHTML == '❌') {
+//             Q.lastElementChild.innerHTML = '➖'
+//         } else {
+//             Q.lastElementChild.innerHTML = '❌'
+//         }
+//         Q.nextElementSibling.classList.toggle('showans');
+//     });
+// });
+
+// let allQ = document.querySelectorAll('.questions h2');
+
+// // second way
+
+// allQ.forEach((Questions, Index) => {
+//     console.log(Questions, Index);
+
+//     Questions.addEventListener('click', () => {
+//         allQ.forEach((q, i) => {
+//             console.log(q, 'click', i, Index);
+//             if (i != Index) {
+//                 q.lastElementChild.innerHTML = '❌';
+//                 q.nextElementSibling.style.display = 'none';
+//             } else {
+//                 q.nextElementSibling.style.display = 'block';
+//             }
+
+//         })
+
+//         if (Questions.lastElementChild.innerHTML == '❌') {
+//             Questions.lastElementChild.innerHTML = '✔';
+//             Questions.nextElementSibling.style.display = 'block';
+//         } else {
+//             Questions.lastElementChild.innerHTML = '❌';
+//             Questions.nextElementSibling.style.display = 'none';
+//         }
+
+//     });
+
+// });
 
